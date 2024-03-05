c74edab26c1fad7fa53c48b846ea2ad1535119ba
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-05 12:32:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//making calculator with use of operator 
+//making variables 
+var num1 = 15;
+var num2 = 3;
+//addition
+var add = (num1 + num2);
+console.log(add);
+//subtraction
+var sub = (num1 - num2);
+console.log(sub);
+//multipication
+var mul = (num1 * num2);
+console.log(mul);
+//division
+var div = (num1 / num2);
+console.log(div);
+//modulus
+var mod = (num1 % num2);
+console.log(mod);
+//exponentiation
+var exp = (Math.pow(num1, num2));
+console.log(exp);
---
+++ main.ts
@@ -0,0 +1,23 @@
+//making calculator with use of operator 
+//making variables 
+let num1:number=15
+let num2:number=3
+
+//addition
+let add=(num1+num2)
+console.log(add)
+//subtraction
+let sub=(num1-num2)
+console.log(sub)
+//multiplication
+let mul=(num1*num2)
+console.log(mul)
+//division
+let div=(num1/num2)
+console.log(div)
+//modulus
+let mod=(num1%num2)
+console.log(mod)
+//exponentiation
+let exp=(num1**num2)
+console.log(exp)
